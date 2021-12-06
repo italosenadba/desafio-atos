2b508e8f44e92d7dbc6e3213d27e2696ad82784c
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2021-12-06 11:05:10 -0300

    Teste Commit

--- cliente/src/main/java/net/atos/api/cliente/service/AtualizaClienteService.java
+++ cliente/src/main/java/net/atos/api/cliente/service/AtualizaClienteService.java
@@ -1,5 +1,24 @@
 package net.atos.api.cliente.service;
 
+import java.util.Scanner;
+
 public class AtualizaClienteService {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N < 0) {
+            System.out.println("NEGATIVO");
+        }
+        else {
+            System.out.println("NAO NEGATIVO");
+        }
+
+        sc.close();
+    }
+}
 
 }
